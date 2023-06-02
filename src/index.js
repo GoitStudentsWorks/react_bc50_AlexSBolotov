57e5b384ca694ccb82e88631ff5d64e37a2fee81
GoitStudentsWorks <129747846+GoitStudentsWorks@users.noreply.github.com>
2023-06-02 18:08:47 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter basename="/react-bc50-1-budget-boss">
+      <BrowserRouter basename="/react_bc50_AlexSBolotov">
         <App />
       </BrowserRouter>
     </PersistGate>
